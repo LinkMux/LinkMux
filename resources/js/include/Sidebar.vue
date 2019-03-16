c84ee32988c6203751a16edf69177fe880ce0afe
Yuuna <k9532121@gmail.com>
2019-03-16 17:19:29 +0800

    Add sidebar can extend.

--- resources/js/include/Sidebar.vue
+++ resources/js/include/Sidebar.vue
@@ -1,9 +1,25 @@
 <template>
-    <div class="sidebar">
-        <p><font-awesome-icon icon="bars"/></p>
-        <p><font-awesome-icon icon="bell"/></p>
-        <p><font-awesome-icon icon="bomb"/></p>
-        <p><font-awesome-icon icon="bolt"/></p>
+    <div id="sidebar" class="sidebar">
+        <div class="sidebar-menu">
+            <ul>
+                <li @click="changeSidebarExtend">
+                    <font-awesome-icon class="icon" icon="bars"/>
+                    <span class="sidebar-menu-text"> Menu </span>
+                </li>
+                <li>
+                    <font-awesome-icon class="icon" icon="bell"/>
+                    <span class="sidebar-menu-text"> Bell </span>
+                </li>
+                <li>
+                    <font-awesome-icon class="icon" icon="bomb"/>
+                    <span class="sidebar-menu-text"> Bomb!! </span>
+                </li>
+                <li>
+                    <font-awesome-icon class="icon" icon="bolt"/>
+                    <span class="sidebar-menu-text"> Bolt </span>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -11,11 +27,22 @@
     export default {
         data() {
             return {
-
+                sidebarExtend: false
             }
         },
         methods: {
-
+            changeSidebarExtend() {
+                let self = this;
+                if (self.sidebarExtend) {
+                    // 移除展開 Menu
+                    self.sidebarExtend = false;
+                    $("#sidebar").removeClass("sidebar-extend");
+                } else {
+                    // 展開 Menu
+                    self.sidebarExtend = true;
+                    $("#sidebar").addClass("sidebar-extend");
+                }
+            }
         },
         computed: {
 
@@ -35,20 +62,63 @@
 <style scoped lang="scss">
     @import '../../sass/variables';
     .sidebar {
-        width: 60px;
+        width: $sidebar-width;
         height: $sidebar-height;
         background: black;
-        color: #bbbbbb;
-        font-size: 24px;
-        text-align: center;
+        font-size: 18px;
+        line-height: 18px;
         position: absolute;
-        p {
-            height: 35px;
-            margin: 20px 0px;
 
-            &:first-child {
-                margin-top: 10px;
+        .sidebar-menu {
+            width: 100%;
+            ul, li {
+                border: 0;
+                margin: 0;
+                padding: 0;
+                text-decoration: none;
+                color: #999999;
             }
+            li {
+                padding: 15px 0px;
+
+                .icon {
+                    width: $sidebar-width;
+                    text-align: center;
+                }
+
+                .sidebar-menu-text {
+                    text-align: left;
+                    display: none;
+                }
+                
+                &:hover {
+                    width: $sidebar-extend-width;
+                    background: #666666;
+                    color: #FFFFFF;
+                    cursor: pointer;
+
+                    @extend %sidebar-hover;
+                }
+            }
+        }
+    }
+
+    .sidebar-extend {
+        width: $sidebar-extend-width;
+
+        .sidebar-menu li {
+            @extend %sidebar-hover;
+        }
+    }
+
+    %sidebar-hover {
+        .icon {
+            width: $sidebar-width;
+        }
+
+        .sidebar-menu-text {
+            width: calc(#{$sidebar-extend-width} - #{$sidebar-width});
+            display: inline !important;
         }
     }
 </style>
